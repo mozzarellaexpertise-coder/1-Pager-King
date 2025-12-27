a3a390631b583349fde4163c35df3d908ff4d0c1
GAT <mozzarellaexpertise@gmail.com>
2025-12-27 17:32:13 +0630

    hard delete firebaseAdmin to break vercel cache

--- src/lib/server/firebaseAdmin.ts
+++ src/lib/server/firebaseAdmin.ts
@@ -1,10 +1,17 @@
-// firebaseAdmin.ts
 import admin from "firebase-admin";
+
+const firebasePrivateKey = process.env.FIREBASE_PRIVATE_KEY;
+
+if (!firebasePrivateKey) {
+  throw new Error("FIREBASE_PRIVATE_KEY is not set");
+}
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_PRIVATE_KEY!))
+    credential: admin.credential.cert(
+      JSON.parse(firebasePrivateKey)
+    )
   });
 }
 
-export default admin;
+export const firebaseAdmin = admin;
